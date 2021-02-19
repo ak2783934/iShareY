5512b06a2869d83034a14e99331e6e2c42f248f7
Avinash kumar <ak2783934@gmail.com>
2021-02-19 15:46:03 +0530

    After changes in server

--- backend/server.js
+++ backend/server.js
@@ -50,12 +50,15 @@ app.use("/api", taskRoutes);
 app.use("/api", workRoutes);
 
 //hosting use
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("../client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/build/index.html"));
-  });
-}
+
+app.use(express.static("public"));
+
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("../client/build"));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../client/build/index.html"));
+//   });
+// }
 
 //listening here
 app.listen(PORT, console.log(`Server is starting at port ${PORT}`));
